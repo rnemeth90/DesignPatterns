383ab3999211b74fbce1b0f7d184a2e015056da1
Ryan Nemeth <ryan.nemeth@aprimo.com>
2022-07-19 09:44:06 -0400

    updates

---
+++ Creational/Singleton/SingletonClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class SingletonClass
+    {
+        private static SingletonClass _instance;
+        private static readonly object _instanceLock = new object();
+
+        public string Value { get; set; }
+
+        private SingletonClass()
+        {
+
+        }
+
+        public static SingletonClass GetInstance(string value)
+        {
+            if (_instance == null)
+            {
+                lock (_instanceLock)
+                { 
+                    if(_instance == null)
+                    {
+                        _instance = new SingletonClass();
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
